b82b9d1ec6efa02ee83f6d053262179bc9aa9366
BruceWu2001 <zijian.wu.2022@scis.smu.edu.sg>
2024-01-29 11:32:03 +0800

    feat:uploadthing backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 const config = require('./utils/config');
 const cors = require('cors');
+const { uploadRouter } = require("./routes/uploadRoutes")
+const { createUploadthingExpressHandler } = require("uploadthing/express");
+// const multer = require('multer');
 
 //Server settings
 const express = require('express');
 const app = express();
 
 //Server params
+const baseUrl = `http://localhost:${config.PORT}`;
 const PORT = config.PORT;
 const API_VER = config.API_VER;
 
 app.use(cors());
 //For parsing application/json data
 app.use(express.json());
+
+
+// would be useful if you want to pass form data
+
+// const upload = multer({dest: "uploads/"})
+// const middleware = upload.single('image')
+// app.use(middleware)
 
 //For parsing application/x-www-form-urlencoded data
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +36,6 @@ const seedData = require('./utils/dbInit');
 //Initialise associations
 // const initAssociations = require('./models/associations');
 
-app.use('/', (req, res) => res.status(200).json("all good :)"))
 
 // //Account routes
 // app.use(API_VER, require('./routes/accountRoutes'))
@@ -35,6 +45,22 @@ app.use('/', (req, res) => res.status(200).json("all good :)"))
 // app.use(API_VER + '/organisers', require('./routes/organiserRoutes'));
 // //Programme routes
 // app.use(API_VER + '/programmes', require('./routes/programmeRoutes'));
+
+
+// uploadthing creates signed s3 url and give to frontend
+// frontend uploads straight to url without passing the image to us
+// they only give a name,size and reserve a slot in s3 for the image
+app.use("/api/uploadthing", createUploadthingExpressHandler({
+    router: uploadRouter,
+    config: {
+        callbackUrl: baseUrl + "/api/uploadthing/link",
+        uploadthingId : config.UPLOADTHING_APP_ID,
+        uploadthingSecret : config.UPLOADTHING_SECRET,
+        }
+    })
+);
+
+app.use('/', (req, res) => res.status(200).json("all good :)"))
 
 sequelize
   .authenticate()
---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,33 @@
+const { createUploadthing } =  require("uploadthing/server");
+
+const f = createUploadthing({
+  /**
+   * Log out more information about the error, but don't return it to the client
+   * @see https://docs.uploadthing.com/errors#error-formatting
+   */
+  errorFormatter: (err) => {
+    console.log("Error uploading file", err.message);
+    console.log("  - Above error caused by:", err.cause);
+
+    return { message: err.message };
+  },
+});
+
+
+const uploadRouter = {
+  videoAndImage: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 4,
+    },
+    video: {
+      maxFileSize: "16MB",
+    },
+  }).onUploadComplete((data) => {
+    console.log("upload completed", data);
+  }),
+}
+
+module.exports = {uploadRouter}
+
+// videoAndImage here is the slug, aka the routes
