f7d9e99104db3fa9c7bd57cdbdefcf922d38e8f4
yutinggh <huang.yu.tingting@gmail.com>
2024-02-28 11:59:09 +0800

    feat: progress view

--- frontend/app/(tabs)/scanface.tsx
+++ frontend/app/(tabs)/scanface.tsx
@@ -1,37 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
-import Slider from "@react-native-community/slider";
+import { StyleSheet, View } from 'react-native';
+import { Text } from '@/components/Themed';
 
 export default function ScanFaceScreen() {
-  const [sliderValue, setSliderValue] = useState(0);
+  const [progress, setProgress] = useState(0); // Initial progress
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Increment the slider value by 1 until it reaches 100
-      if (sliderValue < 100) {
-        setSliderValue(prevValue => prevValue + 1);
+      if (progress < 100) {
+        setProgress(prevProgress => prevProgress + 1);
       } else {
-        // If slider value reaches 100, clear the interval
         clearInterval(interval);
       }
-    }, 50); // Adjust the interval duration as needed
-    // Cleanup function to clear the interval when component unmounts
+    }, 50);
     return () => clearInterval(interval);
-  }, [sliderValue]);
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Scan Face</Text>
-      <Text>Test</Text>
-      <Slider
-        value={sliderValue}
-        minimumValue={0}
-        maximumValue={100}
-        minimumTrackTintColor='#66ff66' // Light green color
-      />
-      <Text>Slider that automatically slides from 0 to 100</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={styles.backgroundView}>
+        <View style={[styles.progressView, { width: `${progress}%` }]} />
+      </View>
     </View>
   );
 }
@@ -50,5 +40,17 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  backgroundView: {
+    height: 20, // Height of the container view
+    width: '75%', // Full width
+    backgroundColor: '#0D332A', // Initial gray color
+    borderRadius: 10, // Border radius to make it rounded
+    overflow: 'hidden', // Clip child view
+    margin: 20,
+  },
+  progressView: {
+    height: '100%', // Full height of the container view
+    backgroundColor: '#D1E543', // Green color
   },
 });
