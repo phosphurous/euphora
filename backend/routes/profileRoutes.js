5da1b73394ed1a72afb7db25d611e46285283366
phosphurous <122954378+phosphurous@users.noreply.github.com>
2024-03-05 15:55:18 +0800

    feat: Added Add Profile and Update Profile

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,14 +1,18 @@
 
 const express = require('express');
 const router = express.Router();
-const {get_all_allergies, get_profile_from_id, get_all_routines} = require('../controllers/profileController');
+const profileController = require('../controllers/profileController');
 
 //AUTHENTICATED ROUTES
 // router.post('/change-password', authenticateToken, accountController.changePassword);
 
 //UNAUTHENTICATED ROUTES
-router.get('/:id/all_allergies', get_all_allergies)
-router.get('/:id/get_routines', get_all_routines)
-router.get('/:id/get_profile', get_profile_from_id)
+router.get('/get_skin_types_cond', profileController.get_skin_types_cond)
+router.post('/add', profileController.add_profile)
+router.put('/update', profileController.update_skin_type_condition)
+
+router.get('/:id/all_allergies', profileController.get_all_allergies)
+router.get('/:id/get_routines', profileController.get_all_routines)
+router.get('/:id/get_profile', profileController.get_profile_from_id)
 
 module.exports = router;
