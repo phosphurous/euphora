b03317420a888602a17a5de8ac427bb43d32416e
yutinggh <huang.yu.tingting@gmail.com>
2024-03-05 13:23:37 +0800

    feat: navigate from Home to specifyCondition

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx or index.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './Navigation'; // Import the Navigation component
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Pressable, TouchableHighlight } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Link } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+        <Link href = "../specifyCondition" asChild>
+            <TouchableHighlight>
+                <Text style={styles.buttonText}>Specify Condition</Text>
+            </TouchableHighlight>
+        </Link>
     </View>
   );
 }
@@ -27,5 +34,15 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#007AFF',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 16,
   },
 });
